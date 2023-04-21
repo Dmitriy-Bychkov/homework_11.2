3d19ce4a36f0e8cbd7db3d6ece409e736c0160c6
Dmitriy Bychkov <582620@gmail.com>
2023-04-21 20:31:13 +0800

    Add new function for make the first letter uppercase

--- str_func.py
+++ str_func.py
@@ -2,8 +2,20 @@
 def upper_letters(string):
     '''
     Converting letters to uppercase
+
+    Добавление забытого докстринга
     '''
     return str(string).upper()
+
+
+def title_letters(string):
+    '''
+    This function makes the first letter uppercase
+    '''
+
+    return str(string).title()
+
+
 
 
 
