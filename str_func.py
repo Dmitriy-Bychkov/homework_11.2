9a6200b7905fa01fd82f3c982f86b7dc191f3ab1
Dmitriy-Bychkov <131114125+Dmitriy-Bychkov@users.noreply.github.com>
2023-04-21 20:19:04 +0800

    Update str_func.py
    Add docstring to function

--- str_func.py
+++ str_func.py
@@ -2,6 +2,10 @@
 def upper_letters(string):
     '''
     Converting letters to uppercase
+    
+    Так как я во втором задании уже написал функцию с докстрингом, 
+    поэтому в 3 задании я осталяю здесь этот текст, в качестве выполнения этой работы.
+    
     '''
     return str(string).upper()
 
